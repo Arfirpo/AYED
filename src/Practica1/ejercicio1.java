097058884395e27ce4e1fb7bd1ef90549841fe99
Agustín Firpo <agustin_firpo@outlook.com.ar>
2025-04-23 12:58:10 -0300

    cree repositorio carpeta AYED

---
+++ src/Practica1/ejercicio1.java
@@ -0,0 +1,13 @@
+package Practica1;
+
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Imprimir desde a hasta b con un for: ");
+        Clase1.conFor(5, 15);
+        System.out.println("Imprimir con un while:");
+        Clase1.conWhile(2, 20);
+        System.out.println("Imprimir recursivamente:");
+        Clase1.recursivo(1, 5);
+    }
+}
