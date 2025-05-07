ecf04f5a4afda04a028ce95c3e36878aecb09945
Agustin Firpo <agustin_@outlook.com.ar>
2025-05-06 21:29:32 -0300

    empezar ejercicio 5 - practica 2

---
+++ src/Practica2/Ejercicio4/RedBinariaLlena.java
@@ -0,0 +1,67 @@
+package Practica2.Ejercicio4;
+
+import Practica1.Ejercicio8.Queue;
+import Practica2.Ejercicio1y2.BinaryTree;
+
+public class RedBinariaLlena {
+
+  public static int retardoReenvio(BinaryTree<Integer> ab) {
+    int maxReenvio = 0;
+    int reenvioNivel = 0;
+    if (ab.isEmpty())
+      return -1;
+    BinaryTree<Integer> nodoActual = null;
+    Queue<BinaryTree<Integer>> cola = new Queue<BinaryTree<Integer>>();
+    cola.enqueue(ab);
+    cola.enqueue(null);
+    while (!cola.isEmpty()) {
+      nodoActual = cola.dequeue();
+      if (nodoActual != null) {
+        if (nodoActual.getData() > reenvioNivel)
+          reenvioNivel = nodoActual.getData();
+        if (nodoActual.hasLeftChild())
+          cola.enqueue(nodoActual.getLeftChild());
+        if (nodoActual.hasRightChild())
+          cola.enqueue(nodoActual.getRightChild());
+      } else {
+        maxReenvio += reenvioNivel;
+        reenvioNivel = -1;
+        if (!cola.isEmpty()) {
+          cola.enqueue(null);
+        }
+      }
+    }
+    return maxReenvio;
+  }
+
+  public static void main(String[] args) {
+    BinaryTree<Integer> arbol = new BinaryTree<Integer>(10);
+
+    BinaryTree<Integer> hijoIzquierdo = new BinaryTree<Integer>(2);
+
+    hijoIzquierdo.addLeftChild(new BinaryTree<Integer>(5));
+    hijoIzquierdo.addRightChild(new BinaryTree<Integer>(4));
+
+    hijoIzquierdo.getLeftChild().addLeftChild(new BinaryTree<Integer>(7));
+    hijoIzquierdo.getLeftChild().addRightChild(new BinaryTree<Integer>(8));
+
+    hijoIzquierdo.getRightChild().addLeftChild(new BinaryTree<Integer>(5));
+    hijoIzquierdo.getRightChild().addRightChild(new BinaryTree<Integer>(6));
+
+    BinaryTree<Integer> hijoDerecho = new BinaryTree<Integer>(3);
+
+    hijoDerecho.addLeftChild(new BinaryTree<Integer>(9));
+    hijoDerecho.addRightChild(new BinaryTree<Integer>(8));
+
+    hijoDerecho.getLeftChild().addLeftChild(new BinaryTree<Integer>(12));
+    hijoDerecho.getLeftChild().addRightChild(new BinaryTree<Integer>(8));
+
+    hijoDerecho.getRightChild().addLeftChild(new BinaryTree<Integer>(2));
+    hijoDerecho.getRightChild().addRightChild(new BinaryTree<Integer>(1));
+
+    arbol.addLeftChild(hijoIzquierdo);
+    arbol.addRightChild(hijoDerecho);
+
+    System.out.println(retardoReenvio(arbol));
+  }
+}
