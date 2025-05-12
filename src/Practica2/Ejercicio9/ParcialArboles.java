c24239c82e51f1b11adfdb0ef8a58662bb512d1e
Agustín Firpo <agustin_firpo@outlook.com.ar>
2025-05-11 21:18:53 -0300

    ejercicio 9 practica 2 en proceso

--- src/Practica2/Ejercicio9/ParcialArboles.java
+++ src/Practica2/Ejercicio9/ParcialArboles.java
@@ -8,27 +8,52 @@ public class ParcialArboles {
 
   public BinaryTree<ArrayList<Integer>> sumAndDif(BinaryTree<Integer> arbol) {
     if (arbol != null && !arbol.isEmpty()) {
-      BinaryTree<ArrayList<Integer>> arbol2 = new BinaryTree<ArrayList<Integer>>();
-      return sumAndDifRec(arbol, arbol2, 0);
+      ArrayList<Integer> listaRaiz = new ArrayList<Integer>();
+      listaRaiz.add(arbol.getData());
+      listaRaiz.add(arbol.getData());
+      BinaryTree<ArrayList<Integer>> arbol2 = new BinaryTree<ArrayList<Integer>>(listaRaiz);
+      if (arbol.hasLeftChild())
+        sumAndDifRec(arbol.getLeftChild(), arbol2, arbol.getData(), true);
+      if (arbol.hasRightChild())
+        sumAndDifRec(arbol.getRightChild(), arbol2, arbol.getData(), false);
+      return arbol2;
     }
     return null;
   }
 
-  private BinaryTree<ArrayList<Integer>> sumAndDifRec(BinaryTree<Integer> ab1, BinaryTree<ArrayList<Integer>> ab2,
-      int ancestro) {
-    if (ab1 != null && ab1.isEmpty()) {
-      ab2.getData().add(ab1.getData() + ancestro);
-      ab2.getData().add(ab1.getData() - ancestro);
-      ancestro = ab1.getData();
+  private void sumAndDifRec(BinaryTree<Integer> ab1, BinaryTree<ArrayList<Integer>> ab2,
+      int ancestro, boolean esHijoIzquierdo) {
+    if (ab1 == null || ab1.isEmpty())
+      return;
 
-      if (ab1.hasLeftChild())
-        ab2.addLeftChild(new BinaryTree<ArrayList<Integer>>());
-      sumAndDifRec(ab1.getLeftChild(), ab2.getLeftChild(), ancestro);
-      if (ab1.hasRightChild())
-        ab2.addRightChild(new BinaryTree<ArrayList<Integer>>());
-      sumAndDifRec(ab1.getRightChild(), ab2.getRightChild(), ancestro);
+    ArrayList<Integer> lista = new ArrayList<Integer>();
+    lista.add(ab1.getData() + ancestro);
+    lista.add(ab1.getData() - ancestro);
+
+    BinaryTree<ArrayList<Integer>> nodoActual = new BinaryTree<ArrayList<Integer>>(lista);
+
+    if (esHijoIzquierdo) {
+      ab2.addLeftChild(nodoActual);
+    } else {
+      ab2.addRightChild(nodoActual);
     }
-    return ab2;
+    if (ab1.hasLeftChild())
+      sumAndDifRec(ab1.getLeftChild(), nodoActual, ab1.getData(), true);
+    if (ab1.hasRightChild())
+      sumAndDifRec(ab1.getRightChild(), nodoActual, ab1.getData(), false);
+  }
+
+  public void imprimirArbolListas(BinaryTree<ArrayList<Integer>> ab) {
+    if (ab != null && !ab.isEmpty()) {
+
+      ab.getData().forEach(nodo -> System.out.print(nodo + " | "));
+      System.out.println();
+
+      if (ab.hasLeftChild())
+        imprimirArbolListas(ab.getLeftChild());
+      if (ab.hasRightChild())
+        imprimirArbolListas(ab.getRightChild());
+    }
   }
 
   public static void main(String[] args) {
@@ -46,8 +71,12 @@ public class ParcialArboles {
     hD.getLeftChild().addRightChild(new BinaryTree<Integer>(4));
     hD.getLeftChild().getRightChild().addRightChild(new BinaryTree<Integer>(6));
 
+    arbol.addLeftChild(hI);
+    arbol.addRightChild(hD);
+
     ParcialArboles parcial = new ParcialArboles();
     BinaryTree<ArrayList<Integer>> arbol2 = parcial.sumAndDif(arbol);
+    parcial.imprimirArbolListas(arbol2);
   }
 
 }
