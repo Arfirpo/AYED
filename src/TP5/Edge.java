343ff261b7a72b88757b3cce6e36a98b662bcf40
Agustin Firpo <agustin_@outlook.com.ar>
2025-07-08 19:42:10 -0300

    agregue estructura grafo

---
+++ src/TP5/Edge.java
@@ -0,0 +1,17 @@
+package TP5;
+
+public interface Edge<T> {
+  	/**
+	 * Retorna el vértice destino de la arista.
+	 * 
+	 * @return
+	 */
+	public Vertex<T> getTarget();
+	
+	/**
+	 * Retorna el peso de la arista
+	 * 
+	 * @return
+	 */
+	public int getWeight();
+}
